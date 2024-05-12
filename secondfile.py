eb362bf2838059b169abe7465b6ac9fc0462c4a7
fletch <iknowfletchlearn@gmail.com>
2024-05-12 09:37:20 -0400

    Revert "PUtting in conflict"
    This reverts commit bedbd3fbd9c863bf69b0453f5bf7f668b1872446.

--- secondfile.py
+++ secondfile.py
@@ -4,5 +4,3 @@ print("This is added to the feature branceh")
 print("Fletch Added this.")
 
 print("THis change was for learning how tgo do pull request")
-
-print(" I added more code, but in the master, someone deleted line 6 in a conflict")
