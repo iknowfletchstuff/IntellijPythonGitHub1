95e53e5322c8e551afb4d2f8f7aa6ff318b69861
fletch <iknowfletchlearn@gmail.com>
2024-05-12 09:42:08 -0400

    Conflilct # 2

--- secondfile.py
+++ secondfile.py
@@ -6,3 +6,5 @@ print("Fletch Added this.")
 print("THis change was for learning how tgo do pull request")
 
 print(" I added more code, but in the master, someone deleted line 6 in a conflict")
+
+print(" this is another test ")
