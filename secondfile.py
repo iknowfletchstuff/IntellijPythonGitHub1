bedbd3fbd9c863bf69b0453f5bf7f668b1872446
fletch <iknowfletchlearn@gmail.com>
2024-05-12 09:27:31 -0400

    PUtting in conflict

--- secondfile.py
+++ secondfile.py
@@ -4,3 +4,5 @@ print("This is added to the feature branceh")
 print("Fletch Added this.")
 
 print("THis change was for learning how tgo do pull request")
+
+print(" I added more code, but in the master, someone deleted line 6 in a conflict")
